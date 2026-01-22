b1a87c11f8d981c9e2fd6496bb6541ac6ac7cf49
fruffini <filruf1702@gmail.com>
2026-01-22 17:24:43 +0100

    esempio per correggere il codice di llava per ereditare le funzioni di base e modificarle

--- src/benchmark/models/llava-rad/__init__.py
+++ src/benchmark/models/llava-rad/__init__.py
@@ -3,6 +3,30 @@ from ..spec import ModelSpec
 
 
 class LLaVARad(BaseHFLM):
+    def _ensure_loaded(self):
+        # Load processor
+        if self._processor is None:
+
+            self._processor = load_processor_llava(
+                self.model_id,
+                cache_dir=self.cache_dir,
+                torch_dtype=self._torch_dtype,
+            )
+            
+        # Load model
+        if self._model is None:
+            
+            self._model = load_model_llava(
+                self.model_id,
+                cache_dir=self.cache_dir,
+                torch_dtype=self._torch_dtype,
+            )
+
+
+        if self.device is not None:
+            self._model.to(self.device)
+
+        return self._model, self._processor
     pass
 
 
